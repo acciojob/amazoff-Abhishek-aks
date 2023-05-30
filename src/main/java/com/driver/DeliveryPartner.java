81f10d2198eb90aae8fd27582a84c4cf8761834f
Abhishek-aks <117778085+Abhishek-aks@users.noreply.github.com>
2023-05-30 18:29:53 +0530

    redone

--- src/main/java/com/driver/DeliveryPartner.java
+++ src/main/java/com/driver/DeliveryPartner.java
@@ -1,26 +1,22 @@
 package com.driver;
 
 public class DeliveryPartner {
-
     private String id;
     private int numberOfOrders;
 
+    // Constructor
     public DeliveryPartner() {
+    }
+    public DeliveryPartner(String id) {
         this.id = id;
         this.numberOfOrders = 0;
     }
 
-    public String getId() {
-        return id;
-    }
-
+    // Getters & Setters
+    public String getId() { return id; }
     public void setId(String id) { this.id = id; }
 
-    public Integer getNumberOfOrders(){
-        return numberOfOrders;
-    }
-
-    public void setNumberOfOrders(Integer numberOfOrders) {
-        this.numberOfOrders = numberOfOrders;
-    }
+    public Integer getNumberOfOrders(){ return numberOfOrders; }
+    public void setNumberOfOrders(Integer numberOfOrders) { this.numberOfOrders = numberOfOrders; }
 }
+
--- src/main/java/com/driver/Order.java
+++ src/main/java/com/driver/Order.java
@@ -1,23 +1,26 @@
 package com.driver;
 
 public class Order {
-
     private String id;
     private int deliveryTime;
+    // Constructor
+    public Order() {
+    }
 
     public Order(String id, String deliveryTime) {
         this.id = id;
         // The deliveryTime has to converted from string to int and then stored in the attribute
         //deliveryTime  = HH*60 + MM
-        String [] sep = deliveryTime.split(":");
-        int time = (Integer.parseInt(sep[0])*60)+ (Integer.parseInt(sep[1]));
+        String [] sep = deliveryTime.split(":");	// Split method return array of strings
+        int time = (Integer.parseInt(sep[0])*60)+ (Integer.parseInt(sep[1])); // Integer.parseInt() method converts String to int
         this.deliveryTime = time;
     }
+    // Getter & Setter
+    public String getId() { return id; }
+    public int getDeliveryTime() { return deliveryTime; }
 
-    public String getId() {
-        return id;
-    }
-
-    public int getDeliveryTime() {return deliveryTime;}
+    public void  setId(String id) { this. id = id;}
+    public void  setDeliveryTime(int deliverytime) { this. deliveryTime = deliverytime;}
 }
+
 
